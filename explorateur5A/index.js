36c4cb63df876b7badeedcb2f9cc7545a468f0ef
AdrienBergerESILV <adrien.berger.31@gmail.com>
2018-01-24 23:30:35 +0100

    Correction Majeur de Style

--- explorateur5A/index.js
+++ explorateur5A/index.js
@@ -1,16 +1,106 @@
 $(document).ready(function(){
 
 	$(".btn-block-top").click(function(){
-		$.get( "https://api.blockcypher.com/v1/btc/main/blocks/0000000000000000189bba3564a63772107b5673c940c16f12662b3e8546b412", function(data) {
-				var htmlOutput = '';
+		$.get( "https://api.blockcypher.com/v1/btc/main", function(data1) {
+			$.get("https://api.blockcypher.com/v1/btc/main/blocks/"+data1.height, function(data){
+				var htmlOutput = ''; var htmlOutput2 = ''; var htmlOutput3 = '';
 			if(data.error != undefined){
 				htmlOutput += data.error;
+				htmlOutput2 += data.error;
+				htmlOutput3 += data.error;
 			}
 			else{
-				htmlOutput += '<td>'+data.chain+'</td><td>'+data.height+'</td><td>'+data.fees+'</td><td>'+data.bits+'</td><td>'+data.time+'</td><td>'+data.depth+'</td><td>'+data.total+'</td>';
+				htmlOutput += '<td>'+data.chain+'</td><td>'+data.height+'</td><td>'+data.fees+'</td><td>'+data.bits+'</td>';
+				htmlOutput2 += '<td>'+data.hash+'</td>';
+				htmlOutput3 += '<td>'+data.time+'</td><td>'+data.depth+'</td><td>'+data.total+'</td>';
 
 			}
-			$(".results").html( htmlOutput );
+			$(".results1").html( htmlOutput );
+			$(".results2").html( htmlOutput2 );
+			$(".results3").html( htmlOutput3 );
+
 			});
+			
+		});
+	});
+
+	$(".btn-block-height").click(function(){
+		var htmlOutput = ''; var htmlOutput2 = ''; var htmlOutput3 = '';
+		if($(".input-block-height").val() == ""){
+			htmlOutput += "Block Height input is blank. Please fill it.";
+			$(".results1").html( htmlOutput );
+			$(".results2").html( htmlOutput );
+			$(".results3").html( htmlOutput );
+		}
+		else{
+			$.get( "https://api.blockcypher.com/v1/btc/main/blocks/"+$(".input-block-height").val(), function(data) {
+				if(data.error != undefined){
+					htmlOutput += data.error;
+					htmlOutput2 += data.error;
+					htmlOutput3 += data.error;
+				}
+				else{
+					htmlOutput += '<td>'+data.chain+'</td><td>'+data.height+'</td><td>'+data.fees+'</td><td>'+data.bits+'</td>';
+					htmlOutput2 += '<td>'+data.hash+'</td>';
+					htmlOutput3 += '<td>'+data.time+'</td><td>'+data.depth+'</td><td>'+data.total+'</td>';
+				}
+				$(".results1").html( htmlOutput );
+				$(".results2").html( htmlOutput2 );
+				$(".results3").html( htmlOutput3 );
+			});
+		}
+	});
+
+	$(".btn-address-hash").click(function(){
+		var htmlOutput = ''; var htmlOutput2 = ''; var htmlOutput3 = '';
+		if($(".input-address-hash").val() == ""){
+			htmlOutput += "Address Hash input is blank. Please fill it.";
+			$(".results1").html( htmlOutput );
+			$(".results2").html( htmlOutput );
+			$(".results3").html( htmlOutput );
+		}
+		else{
+			$.get( "https://api.blockcypher.com/v1/btc/main/addrs/"+$(".input-address-hash").val(), function(data) {
+				if(data.error != undefined){
+					htmlOutput += data.error;
+					htmlOutput2 += data.error;
+					htmlOutput3 += data.error;
+				}
+				else{
+					htmlOutput += '<td>'+data.chain+'</td><td>'+data.height+'</td><td>'+data.fees+'</td><td>'+data.bits+'</td>';
+					htmlOutput2 += '<td>'+data.hash+'</td>';
+					htmlOutput3 += '<td>'+data.time+'</td><td>'+data.depth+'</td><td>'+data.total+'</td>';
+				}
+				$(".results1").html( htmlOutput );
+				$(".results2").html( htmlOutput2 );
+				$(".results3").html( htmlOutput3 );
+			});
+		}
+	});
+
+	$(".btn-transaction-hash").click(function(){
+		var htmlOutput = ''; var htmlOutput2 = ''; var htmlOutput3 = '';
+		if($(".input-transaction-hash").val() == ""){
+			htmlOutput += "Transaction Hash input is blank. Please fill it.";
+			$(".results1").html( htmlOutput );
+			$(".results2").html( htmlOutput );
+			$(".results3").html( htmlOutput );		}
+		else{
+			$.get( "https://api.blockcypher.com/v1/btc/main/txs/"+$(".input-transaction-hash").val(), function(data) {
+				if(data.error != undefined){
+					htmlOutput += data.error;
+					htmlOutput2 += data.error;
+					htmlOutput3 += data.error;
+				}
+				else{
+					htmlOutput += '<td>'+data.chain+'</td><td>'+data.height+'</td><td>'+data.fees+'</td><td>'+data.bits+'</td>';
+					htmlOutput2 += '<td>'+data.hash+'</td>';
+					htmlOutput3 += '<td>'+data.time+'</td><td>'+data.depth+'</td><td>'+data.total+'</td>';
+				}
+				$(".results1").html( htmlOutput );
+				$(".results2").html( htmlOutput2 );
+				$(".results3").html( htmlOutput3 );
+			});
+		}
 	});
 });
